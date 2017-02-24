a4259da9b0916e450715f43211478c5a862a491d
Aaron-vk-Yuan <xuanyuanziruo@gmail.com>
2017-02-24 08:25:05 +0800

    add layout 图标

--- app/views/laylout/layout.js
+++ app/views/laylout/layout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Navigator, View} from 'react-native';
+import {StyleSheet, Navigator, View, Image} from 'react-native';
 import TabNavigator from 'react-native-tab-navigator';
 
 import HomeView from '../home/home';
@@ -20,18 +20,24 @@ export default class Layout extends React.Component {
         <TabNavigator.Item
           selected={this.setSelected('home')}
           title="首页"
+          renderIcon={() => <Image source={require('./img/home_gray@3x.png')}/>}
+          renderSelectedIcon={() => <Image source={require('./img/home@3x.png')}/>}
           onPress={() => this.switchTab('home')}>
           <HomeView/>
         </TabNavigator.Item>
         <TabNavigator.Item
           selected={this.setSelected('strategy')}
           title="策略"
+          renderIcon={() => <Image source={require('./img/strategy_gray@3x.png')}/>}
+          renderSelectedIcon={() => <Image source={require('./img/strategy@3x.png')}/>}
           onPress={() => this.switchTab('strategy')}>
           <StrategyView/>
         </TabNavigator.Item>
         <TabNavigator.Item
           selected={this.setSelected('mine')}
           title="我的"
+          renderIcon={() => <Image source={require('./img/mine_gray@3x.png')}/>}
+          renderSelectedIcon={() => <Image source={require('./img/mine@3x.png')}/>}
           onPress={() => this.switchTab('mine')}>
           <MineView/>
         </TabNavigator.Item>
