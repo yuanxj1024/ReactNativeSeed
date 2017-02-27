1a48cceca6e8636405df539f418135eb5a89160e
Aaron-vk-Yuan <xuanyuanziruo@gmail.com>
2017-02-27 09:29:00 +0800

    引入scrollview

--- app/views/home/home.js
+++ app/views/home/home.js
@@ -1,36 +1,18 @@
 import React from 'react';
-import {StyleSheet, Navigator, Text, View} from 'react-native';
-import {
-  Container,
-  Content,
-  Header,
-  StyleProvider,
-  Button,
-  connectStyle
-} from 'native-base';
+import {StyleSheet, Navigator, Text, View, ScrollView} from 'react-native';
 
 import BasicStyle from '../../styles/basic';
 
 import Top from './top';
 
-class Home extends React.Component {
+export default class Home extends React.Component {
   render() {
-    const styles = this.props.style;
     return (
-      <View style={[BasicStyle.content]}>
-        <Top/>
+      <View style={[BasicStyle.flex, BasicStyle.content]}>
+        <ScrollView style={[BasicStyle.flex]}>
+          <Top/>
+        </ScrollView>
       </View>
     );
   }
 }
-
-const styles = {
-  bg1: {
-    backgroundColor: 'red'
-  },
-  bg2: {
-    backgroundColor: 'blue'
-  }
-};
-
-export default connectStyle('home.styles', styles)(Home);
